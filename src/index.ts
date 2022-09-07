121d70b492e57bf53bb4e280da1a91a30099a24a
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-07 01:49:36 -0300

    feat: Aplicando variaveis de ambiente no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import 'dotenv/config';
 import app from './app';
 import connectToDatabase from './connection';
 
 const PORT = process.env.PORT || 3001;
-
 connectToDatabase()
   .then(() => {
     app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
