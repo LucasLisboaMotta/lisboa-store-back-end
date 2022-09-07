e5983f4538c3cf118fbd1c426753587eaf8a8468
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-07 01:57:20 -0300

    feat: Criando middleware de erro

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import CustomError from '../helpers/CustomError';
+
+const errorMiddleware = (
+  error: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (error.status) {
+    return res.status(error.status).json({ error: error.message });
+  }
+  return res.status(500).json({ message: 'Internal server error' });
+};
+
+export default errorMiddleware;
